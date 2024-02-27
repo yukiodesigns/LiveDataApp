209a205aff2fd24d11bd52ab5c8abd7cb213078e
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-27 13:51:29 +0300

    Observe live data

--- app/src/main/java/com/example/livedataapp/MainActivity.kt
+++ app/src/main/java/com/example/livedataapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.livedataapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.livedataapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding :ActivityMainBinding
+    lateinit var model :MainActivityViewModel
+    lateinit var factory: MainActivityViewModelFactory
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        factory = MainActivityViewModelFactory(200)
+        model = ViewModelProvider(this, factory).get(MainActivityViewModel::class.java)
+
+        //Observing for data change
+        model.totalSum.observe(this, Observer {
+                binding.resultTxt.text = it.toString() }
+        )
+
+        binding.btn.setOnClickListener(){
+            model.sumUp(binding.editText.text.toString().toInt())
+        }
     }
 }
--- app/src/main/java/com/example/livedataapp/MainActivityViewModelFactory.kt
+++ app/src/main/java/com/example/livedataapp/MainActivityViewModelFactory.kt
@@ -1,7 +1,14 @@
 package com.example.livedataapp
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class MainActivityViewModelFactory(startNum:Int): ViewModelProvider.Factory {
+class MainActivityViewModelFactory(private val startNum:Int): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(MainActivityViewModel::class.java)){
+            return MainActivityViewModel(startNum) as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+    }
 
 }
